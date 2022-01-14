1a8e0ea12934350dde4d97c8c794bfed0876dc9c
Adam Whitehurst <Adam.Whitehurst@live.com>
2022-01-13 16:05:02 -0800

    fix: max-width of main content

--- layouts/base-layout.js
+++ layouts/base-layout.js
@@ -1,3 +1,8 @@
+import styled from 'styled-components'
+const BaseCntnr = styled.div`
+  margin: auto;
+  max-width: 1280px;
+`
 export default function BaseLayout({ children }) {
-  return <div id="base-layout-container">{children}</div>
+  return <BaseCntnr id="base-layout-container">{children}</BaseCntnr>
 }
